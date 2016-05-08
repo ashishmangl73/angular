9426ef896d6673f9c3ae4116e25496ddaafaabf3
ashishmg <ashish.mangla@berylsystems.com>
2016-05-08 07:36:06 +0530

    angualar js master class exe

---
+++ angular master class/excercisesss/03/03/app.js
@@ -0,0 +1,15 @@
+﻿var com = {};
+com.mydomain = {};
+com.mydomain.Aprogram = {};
+com.mydomain.Aprogram.utility = function () {
+    console.log("programmer a");
+
+}
+
+com.mydomain.Aprogram.utility();
+
+
+function utility() {
+    console.log("programmer b");
+}
+utility();
